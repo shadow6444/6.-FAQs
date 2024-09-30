8eeeea269f273257bed20daf31d2daa1c7a9d12e
shadow6444 <shadowgarden644@gmail.com>
2024-09-30 15:30:17 +0500

    Create faqs.js

---
+++ src/faqs.js
@@ -0,0 +1,46 @@
+const faqs = [
+  {
+    id: 1,
+    showAnswer:false,
+    question: "What are components in React?",
+    answer:
+      "React is a JavaScript library for building user interfaces. It uses components to create fast, scalable, and reusable UI elements.",
+  },
+  {
+    id: 2,
+    showAnswer:false,
+    question: "What are components in React?",
+    answer:
+      "Components are the building blocks of React apps. They allow developers to split the UI into independent, reusable pieces.",
+  },
+  {
+    id: 3,
+    showAnswer:false,
+    question: "What is JSX?",
+    answer:
+      "JSX stands for JavaScript XML, allowing you to write HTML-like syntax in JavaScript. It makes your code more readable and easier to manage.",
+  },
+  {
+    id: 4,
+    showAnswer:false,
+    question: "What is a state in React?",
+    answer:
+      "State represents the dynamic parts of your app. It allows components to remember and respond to user interactions or changes over time.",
+  },
+  {
+    id: 5,
+    showAnswer:false,
+    question: "What are props in React?",
+    answer:
+      "Props are short for properties, and they pass data from one component to another. Props are immutable and help make components reusable.",
+  },
+  {
+    id: 6,
+    showAnswer:false,
+    question: "What is the Virtual DOM?",
+    answer:
+      "The Virtual DOM is a lightweight representation of the actual DOM. React updates it first, then efficiently syncs changes with the real DOM.",
+  },
+];
+
+export default faqs;
